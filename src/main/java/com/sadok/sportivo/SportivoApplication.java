00fcced7060bafb7d2e1ba5e4f161aa207a04c26
Mohammed Sadok <mohammed.sadok.2003@gmail.com>
2026-02-21 14:49:55 +0000

    first commit

---
+++ src/main/java/com/sadok/sportivo/SportivoApplication.java
@@ -0,0 +1,13 @@
+package com.sadok.sportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadok/sportivo/SportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadok.sportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
